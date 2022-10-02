c0fffba487f4da3195715a39e0548a72b3f832a8
Sohee <jsh7471@gmail.com>
2022-10-02 12:41:49 -0700

    Update employee.js

--- employee.js
+++ employee.js
@@ -77,7 +77,7 @@ function createHTMLString(item) {
 }
 function employeeListClick(event, employees) {
   const table = document.getElementById("punchInDetails");
-  table.innerHTML = ``;
+  table.innerHTML = ` `;
   const dataset = event.target.dataset;
   const key = dataset.key;
   const value = dataset.value;
